fad62070e857d6bce15d25e8bfc73d7492334047
Mateusz Słabek <mateusz.slabek@protonmail.com>
2023-06-30 22:41:57 +0200

    format config files and add css loader and reactflow

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -2,37 +2,41 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "build"),
+    entry: "./src/index.tsx",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "build"),
     },
-    port: 3000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ["ts-loader"],
-      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
     ],
-  },
-  resolve: {
-    extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
-  }
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "build"),
+        },
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+    },
 };
